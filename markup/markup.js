f3982972ad4c4044c86c5ac8bb7145dcd5cb925d
jeontaesu1 <jts@frameout.co.kr>
2025-01-15 15:40:42 +0900

    Updates

--- markup/markup.js
+++ markup/markup.js
@@ -26,7 +26,7 @@ $(function () {
         str += `<div class="depth_3">${a.depth3 ? a.depth3 : ""}</div>`;
         str += `<div class="info">${a.info ? a.info : ""}</div>`;
         // str += `<div class="fileName"><a href="../html/${a.src.split("?")[0]}.html${a.src.split("?")[1] ? `?` + a.src.split("?")[1] : ""}" target="_blank" >${a.src.split("?")[0]}</a></div>`;
-        str += `<div class="fileName"><a href="${a.src === 'main' ? 'main/main.html' : `../html/${a.src.split("?")[0]}.html${a.src.split("?")[1] ? `?` + a.src.split("?")[1] : ""}`}" target="_blank">${a.src.split("?")[0]}</a></div>`;
+        str += `<div class="fileName"><a href="${a.src === 'main' ? 'main/main.html' : `html/${a.src.split("?")[0]}.html${a.src.split("?")[1] ? `?` + a.src.split("?")[1] : ""}`}" target="_blank">${a.src.split("?")[0]}</a></div>`;
         str += `</li>`;
       });
       str += `</ul>`;
